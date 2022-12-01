4c88189259b934e0f14047827d3c57a841745c98
KID <50666207+KID421@users.noreply.github.com>
2022-12-01 15:03:52 +0800

    Update 對話資料與對話介面設定

---
+++ Unity_ASIA_Game_B_3D_20221124/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+
+namespace KID
+{
+    /// <summary>
+    /// 對話系統
+    /// </summary>
+    public class DialogueSystem : MonoBehaviour
+    {
+        [SerializeField, Header("對話間隔"), Range(0, 0.5f)]
+        private float dialogueIntervalTime = 0.1f;
+
+        private WaitForSeconds dialogueInterval;
+        private CanvasGroup groupDialogue;
+        private TextMeshProUGUI textName;
+        private TextMeshProUGUI textContent;
+        private GameObject goTriangle;
+    }
+}
