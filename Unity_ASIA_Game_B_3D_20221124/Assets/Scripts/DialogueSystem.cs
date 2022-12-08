9a27c87f94861ed4c73c7bd66c8d3a5b8b8a344e
KID <50666207+KID421@users.noreply.github.com>
2022-12-08 15:59:31 +0800

    Update 添加對話系統

--- Unity_ASIA_Game_B_3D_20221124/Assets/Scripts/DialogueSystem.cs
+++ Unity_ASIA_Game_B_3D_20221124/Assets/Scripts/DialogueSystem.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using TMPro;
+using System.Collections;
 
 namespace KID
 {
@@ -30,7 +31,41 @@ namespace KID
             textContent = GameObject.Find("對話內容").GetComponent<TextMeshProUGUI>();
             goTriangle = GameObject.Find("對話完成圖示");
             goTriangle.SetActive(false);
+
+            StartCoroutine(FadeGroup());
+            StartCoroutine(TypeEffect());
         } 
         #endregion
+
+        /// <summary>
+        /// 淡入淡出群組物件
+        /// </summary>
+        private IEnumerator FadeGroup()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                groupDialogue.alpha += 0.1f;
+                yield return new WaitForSeconds(0.04f);
+            }
+        }
+
+        /// <summary>
+        /// 打字效果
+        /// </summary>
+        private IEnumerator TypeEffect()
+        {
+            textName.text = dialogueOpening.dialogueName;
+            textContent.text = "";
+
+            string dialogue = dialogueOpening.dialogueContents[1];
+
+            for (int i = 0; i < dialogue.Length; i++)
+            {
+                textContent.text += dialogue[i];
+                yield return dialogueInterval;
+            }
+
+            goTriangle.SetActive(true);
+        }
     }
 }
